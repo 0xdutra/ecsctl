2eb3c0b39e22be536e66e64348bab370a408ae93
Gabriel M. Dutra <0xdutra@gmail.com>
2022-08-08 11:09:39 -0300

    chore: changing description

--- cmd/root.go
+++ cmd/root.go
@@ -36,15 +36,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ecsctl",
-	Short: "Cli for managing AWS Elastic Container Service",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Cli to manage AWS Elastic Container Service",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -55,16 +50,3 @@ func Execute() {
 		os.Exit(1)
 	}
 }
-/*
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ecsctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-*/
