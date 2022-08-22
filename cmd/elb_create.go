cd49e97588b8110947dac357a9bf2c31f88beabf
Gabriel M. Dutra <0xdutra@gmail.com>
2022-08-22 16:15:09 -0300

    feat: Adding commands to manage load balancers

---
+++ cmd/elb_create.go
@@ -0,0 +1,81 @@
+/*
+Copyright © 2022 Gabriel M. Dutra <0xdutra@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"ecsctl/pkg/provider"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/spf13/cobra"
+)
+
+// elbCreateCmd represents the create command
+var elbCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Commands to create Elastic Load Balancer",
+	Run:   createElbRun,
+}
+
+var (
+	subnets            []string
+	loadBalancerName   string
+	loadBalancerScheme string
+	loadBalancerType   string
+)
+
+func init() {
+	elbCmd.AddCommand(elbCreateCmd)
+	elbCreateCmd.PersistentFlags().StringVarP(&loadBalancerName, "name", "", "", "The name of the Elastic Load Balancer")
+	elbCreateCmd.PersistentFlags().StringVarP(&loadBalancerScheme, "scheme", "", "internet-facing", "Specify a scheme for a Elastic Load Balancer")
+	elbCreateCmd.PersistentFlags().StringArrayVarP(&subnets, "subnet", "", nil, "The list of subnets ids")
+	elbCreateCmd.PersistentFlags().StringVarP(&loadBalancerType, "type", "", "application", "The type of Elastic Load Balancer")
+
+	elbCreateCmd.MarkPersistentFlagRequired("name")
+	elbCreateCmd.MarkPersistentFlagRequired("subnet")
+}
+
+func createElbRun(cmd *cobra.Command, args []string) {
+	var awsSubnets []*string
+	for _, v := range subnets {
+		awsSubnets = append(awsSubnets, aws.String(v))
+	}
+
+	sess := provider.NewSession()
+	svc := elbv2.New(sess)
+
+	input := &elbv2.CreateLoadBalancerInput{
+		Name:    aws.String(loadBalancerName),
+		Subnets: awsSubnets,
+		Type:    aws.String(loadBalancerType),
+		Scheme:  aws.String(loadBalancerScheme),
+	}
+
+	result, err := svc.CreateLoadBalancer(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
+}
---
+++ cmd/elb_create_listener.go
@@ -0,0 +1,125 @@
+/*
+Copyright © 2022 Gabriel M. Dutra <0xdutra@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"ecsctl/pkg/provider"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/spf13/cobra"
+)
+
+// createElbListenerCmd represents the createListener command
+var createElbListenerCmd = &cobra.Command{
+	Use:   "create-listener",
+	Short: "Commands to create Elastic Load Balancer listeners",
+	Run:   createElbListenerRun,
+}
+
+var (
+	loadBalancerListenerArn      string
+	loadBalancerListenerTgArn    string
+	loadBalancerListenerPort     int64
+	loadBalancerListenerProtocol string
+)
+
+func init() {
+	elbCmd.AddCommand(createElbListenerCmd)
+	createElbListenerCmd.PersistentFlags().StringVarP(&loadBalancerListenerArn, "elb-arn", "", "", "The arn of the Elastic Load Balancer")
+	createElbListenerCmd.PersistentFlags().StringVarP(&loadBalancerListenerTgArn, "tg-arn", "", "", "The arn of the Target Group")
+	createElbListenerCmd.PersistentFlags().Int64VarP(&loadBalancerListenerPort, "tg-port", "", 80, "The port of the Target Group")
+	createElbListenerCmd.PersistentFlags().StringVarP(&loadBalancerListenerProtocol, "tg-protocol", "", "HTTP", "The protocol of the Target Group")
+
+	createElbListenerCmd.MarkPersistentFlagRequired("elb-arn")
+	createElbListenerCmd.MarkPersistentFlagRequired("tg-arn")
+}
+
+func createElbListenerRun(cmd *cobra.Command, args []string) {
+	sess := provider.NewSession()
+	svc := elbv2.New(sess)
+
+	input := &elbv2.CreateListenerInput{
+		DefaultActions: []*elbv2.Action{
+			{
+				TargetGroupArn: aws.String(loadBalancerListenerTgArn),
+				Type:           aws.String("forward"),
+			},
+		},
+		LoadBalancerArn: aws.String(loadBalancerListenerArn),
+		Port:            aws.Int64(loadBalancerListenerPort),
+		Protocol:        aws.String(loadBalancerListenerProtocol),
+	}
+
+	result, err := svc.CreateListener(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case elbv2.ErrCodeDuplicateListenerException:
+				fmt.Println(elbv2.ErrCodeDuplicateListenerException, aerr.Error())
+			case elbv2.ErrCodeTooManyListenersException:
+				fmt.Println(elbv2.ErrCodeTooManyListenersException, aerr.Error())
+			case elbv2.ErrCodeTooManyCertificatesException:
+				fmt.Println(elbv2.ErrCodeTooManyCertificatesException, aerr.Error())
+			case elbv2.ErrCodeLoadBalancerNotFoundException:
+				fmt.Println(elbv2.ErrCodeLoadBalancerNotFoundException, aerr.Error())
+			case elbv2.ErrCodeTargetGroupNotFoundException:
+				fmt.Println(elbv2.ErrCodeTargetGroupNotFoundException, aerr.Error())
+			case elbv2.ErrCodeTargetGroupAssociationLimitException:
+				fmt.Println(elbv2.ErrCodeTargetGroupAssociationLimitException, aerr.Error())
+			case elbv2.ErrCodeInvalidConfigurationRequestException:
+				fmt.Println(elbv2.ErrCodeInvalidConfigurationRequestException, aerr.Error())
+			case elbv2.ErrCodeIncompatibleProtocolsException:
+				fmt.Println(elbv2.ErrCodeIncompatibleProtocolsException, aerr.Error())
+			case elbv2.ErrCodeSSLPolicyNotFoundException:
+				fmt.Println(elbv2.ErrCodeSSLPolicyNotFoundException, aerr.Error())
+			case elbv2.ErrCodeCertificateNotFoundException:
+				fmt.Println(elbv2.ErrCodeCertificateNotFoundException, aerr.Error())
+			case elbv2.ErrCodeUnsupportedProtocolException:
+				fmt.Println(elbv2.ErrCodeUnsupportedProtocolException, aerr.Error())
+			case elbv2.ErrCodeTooManyRegistrationsForTargetIdException:
+				fmt.Println(elbv2.ErrCodeTooManyRegistrationsForTargetIdException, aerr.Error())
+			case elbv2.ErrCodeTooManyTargetsException:
+				fmt.Println(elbv2.ErrCodeTooManyTargetsException, aerr.Error())
+			case elbv2.ErrCodeTooManyActionsException:
+				fmt.Println(elbv2.ErrCodeTooManyActionsException, aerr.Error())
+			case elbv2.ErrCodeInvalidLoadBalancerActionException:
+				fmt.Println(elbv2.ErrCodeInvalidLoadBalancerActionException, aerr.Error())
+			case elbv2.ErrCodeTooManyUniqueTargetGroupsPerLoadBalancerException:
+				fmt.Println(elbv2.ErrCodeTooManyUniqueTargetGroupsPerLoadBalancerException, aerr.Error())
+			case elbv2.ErrCodeALPNPolicyNotSupportedException:
+				fmt.Println(elbv2.ErrCodeALPNPolicyNotSupportedException, aerr.Error())
+			case elbv2.ErrCodeTooManyTagsException:
+				fmt.Println(elbv2.ErrCodeTooManyTagsException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
